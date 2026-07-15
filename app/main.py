8fae9dd117e83bb26cc3150ab0e19c012dfe4fb4
seohee <p03061180@gmail.com>
2026-07-15 17:19:48 +0900

    chore: vercel URL 수정

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://townly-fe.vercel.app/",
+        "https://townly-fe.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
